01d5845cad711a6545d6261692b151ad834e0f07
Bill Jackson <billjackson@cakewalk.com>
2018-05-25 07:35:02 -0400

    GetAirTime function. Marker, dotted line..
    Example scene, project dir organization.

--- Assets/Scripts/ArcCast.cs
+++ Assets/Scripts/ArcCast.cs
@@ -8,18 +8,17 @@ public class ArcCast : MonoBehaviour {
         hit = new RaycastHit();
         List<Vector3> arcPositions = new List<Vector3>();
         Vector3 lastLinePos = startPos;
-        float airTime = 5; // TODO: CalculateAirTime(direction, reach, drop) { trajectory formula };
+        float airTime = CalculateAirTime(direction, reach, drop);
         for (int i = 0; i < segments; i++)
         {
             Vector3 nextLinePos = PlotProjectileTrajectory(startPos, direction * reach, drop, Remap(i, 0, segments, 0, airTime));
+            arcPositions.Add(nextLinePos);
             Physics.Raycast(lastLinePos, nextLinePos - lastLinePos, out hit, Vector3.Distance(nextLinePos, lastLinePos));
             if (hit.collider != null)
             {
-                arcPositions.Add(nextLinePos);
                 break;
             }
             lastLinePos = nextLinePos;
-            arcPositions.Add(lastLinePos);
         }
         return arcPositions;
     }
@@ -32,5 +31,14 @@ public class ArcCast : MonoBehaviour {
     private static float Remap(float sourceValue, float sourceMin, float sourceMax, float destMin, float destMax)
     {
         return Mathf.Lerp(destMin, destMax, Mathf.InverseLerp(sourceMin, sourceMax, sourceValue));
+    }
+
+    private static float CalculateAirTime(Vector3 direction, float reach, float drop)
+    {
+        float result = reach * Mathf.Sin((Vector3.Angle(direction, Vector3.forward) * Mathf.PI) / 180) / drop;
+        // Fudge to accommodate start position elevation
+        // An angle of zero (aiming straight) produces 0 seconds.
+        // Don't ever let airtime below 2 seconds; Add 25% extra time to actually reach the ground. 
+        return Mathf.Max(result * 2.5f, 2); 
     }
 }
--- Assets/Scripts/ArcCastExample.cs
+++ Assets/Scripts/ArcCastExample.cs
@@ -5,16 +5,26 @@ using UnityEngine;
 public class ArcCastExample : MonoBehaviour {
 
     public LineRenderer line;
+    public Transform marker;
 	
 	// Update is called once per frame
 	void Update () {
         RaycastHit hit;
         List<Vector3> arcPositions = ArcCast.Cast(transform.position, transform.forward, out hit);
         line.positionCount = arcPositions.Count;
+        print(arcPositions.Count);
         line.SetPositions(arcPositions.ToArray());
         if (hit.collider)
         {
-            //print(hit.collider.name);
+            print(hit.collider.name);
+            if (marker.localScale.magnitude < 0)
+                marker.localScale = Vector3.one;
+            marker.position = hit.point;
+        }
+        else
+        {
+            if (marker.localScale.magnitude > 0)
+                marker.localScale = Vector3.zero;
         }
 	}
 }
